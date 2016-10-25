24f804b324ca4a90165707b03683538d2021a965
J. Xavier Prochaska <xavier@ucolick.org>
2016-10-25 09:44:24 -0700

    plots

--- src/vette_results.py
+++ src/vette_results.py
@@ -6,6 +6,13 @@ from __future__ import print_function, absolute_import, division, unicode_litera
 import numpy as np
 import pdb
 
+
+import matplotlib as mpl
+mpl.rcParams['font.family'] = 'stixgeneral'
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
+
 from astropy.table import Table
 from astropy.coordinates import SkyCoord, match_coordinates_sky
 from astropy import units as u
@@ -106,8 +113,10 @@ def vette_dlasurvey(ml_survey, sdss_survey, fig_root='tmp', lyb_cut=True,
 
     Returns
     -------
-    results : dict
-      Also generates figures
+    false_neg : list
+      List of systems that are false negatives from SDSS -> ML
+    midx : list
+      List of indices matching SDSS -> ML
     """
     from pyigm.surveys import dlasurvey as pyis_ds
     reload(pyis_ds)
@@ -120,6 +129,7 @@ def vette_dlasurvey(ml_survey, sdss_survey, fig_root='tmp', lyb_cut=True,
             # Mask
             mask = pyis_ds.dla_stat(survey, survey.sightlines)
             survey.mask = mask
+        print("Done cutting on Lyb")
 
     # Setup coords
     ml_coords = ml_survey.coord
@@ -136,8 +146,6 @@ def vette_dlasurvey(ml_survey, sdss_survey, fig_root='tmp', lyb_cut=True,
         if len(gd_radec) == 0:
             false_neg.append(isys)
             midx.append(-1)
-            print('here')
-            pdb.set_trace()
         else:
             gdz = np.abs(ml_z[gd_radec] - isys.zabs) < dz_toler
             if np.sum(gdz) > 0:
@@ -146,9 +154,130 @@ def vette_dlasurvey(ml_survey, sdss_survey, fig_root='tmp', lyb_cut=True,
             else:
                 false_neg.append(isys)
                 midx.append(-1)
-                print('no, here')
-                pdb.set_trace()
+    # Return
+    return false_neg, np.array(midx)
 
+
+def fig_dzdnhi(ml_survey, sdss_survey, midx, outfil='fig_dzdnhi.pdf'):
+    """  Compare zabs and NHI between SDSS and ML
+
+    Parameters
+    ----------
+    ml_survey : IGMSurvey
+      Survey describing the Machine Learning results
+      This should be masked according to the vetting
+    sdss_survey : DLASurvey
+      SDSS survey, usually human (e.g. JXP for DR5)
+      This should be masked according to the vetting
+    midx : list
+      List of indices matching SDSS -> ML
+    outfil : str, optional
+      Input None to plot to screen
+
+    Returns
+    -------
+
+    """
+    # z, NHI
+    z_sdss = sdss_survey.zabs
+    z_ml = ml_survey.zabs
+    NHI_sdss = sdss_survey.NHI
+    NHI_ml = ml_survey.NHI
+    # deltas
+    dz = []
+    dNHI = []
+    for qq,idx in enumerate(midx):
+        if idx < 0:
+            continue
+        # Match
+        dz.append(z_sdss[qq]-z_ml[idx])
+        dNHI.append(NHI_sdss[qq]-NHI_ml[idx])
+
+    # Figure
+    if outfil is not None:
+        pp = PdfPages(outfil)
+    fig = plt.figure(figsize=(8, 5))
+    plt.clf()
+    gs = gridspec.GridSpec(1, 2)
+    # dz
+    ax = plt.subplot(gs[0])
+    ax.hist(dz, color='green', bins=20)#, normed=True)#, bins=20 , zorder=1)
+    #ax.text(0.05, 0.74, lbl3, transform=ax.transAxes, color=wcolor, size=csz, ha='left')
+    ax.set_xlim(-0.03, 0.03)
+    ax.set_xlabel(r'$\delta z$ [SDSS-ML]')
+    # NHI
+    ax = plt.subplot(gs[1])
+    ax.hist(dNHI, color='blue', bins=20)#, normed=True)#, bins=20 , zorder=1)
+    #ax.text(0.05, 0.74, lbl3, transform=ax.transAxes, color=wcolor, size=csz, ha='left')
+    #ax.set_xlim(-0.03, 0.03)
+    ax.set_xlabel(r'$\Delta \log N_{\rm HI}$ [SDSS-ML]')
+    #
+    # End
+    plt.tight_layout(pad=0.2,h_pad=0.,w_pad=0.1)
+    if outfil is not None:
+        print('Writing {:s}'.format(outfil))
+        pp.savefig()
+        pp.close()
+        plt.close()
+    else:
+        plt.show()
+
+
+def fig_falseneg(ml_survey, sdss_survey, false_neg, outfil='fig_falseneg.pdf'):
+    """   Figure on false negatives
+
+    Parameters
+    ----------
+    ml_survey : IGMSurvey
+      Survey describing the Machine Learning results
+      This should be masked according to the vetting
+    sdss_survey : DLASurvey
+      SDSS survey, usually human (e.g. JXP for DR5)
+      This should be masked according to the vetting
+    midx : list
+      List of indices matching SDSS -> ML
+    false_neg : list
+      List of false negatives
+    outfil : str, optional
+      Input None to plot to screen
+    outfil
+
+    Returns
+    -------
+
+    """
+    # Generate some lists
+    zabs_false = [isys.zabs for isys in false_neg]
+    zem_false = [isys.zem for isys in false_neg]
+    NHI_false = [isys.NHI for isys in false_neg]
+
+    # Figure
+    if outfil is not None:
+        pp = PdfPages(outfil)
+    fig = plt.figure(figsize=(8, 5))
+    plt.clf()
+    gs = gridspec.GridSpec(2, 2)
+    # zabs
+    ax = plt.subplot(gs[0])
+    ax.hist(zabs_false, color='green', bins=20)#, normed=True)#, bins=20 , zorder=1)
+    ax.set_xlabel(r'$z_{\rm abs}$')
+    # zem
+    ax = plt.subplot(gs[1])
+    ax.hist(zem_false, color='red', bins=20)#, normed=True)#, bins=20 , zorder=1)
+    ax.set_xlabel(r'$z_{\rm qso}$')
+    # NHI
+    ax = plt.subplot(gs[2])
+    ax.hist(NHI_false, color='blue', bins=20)#, normed=True)#, bins=20 , zorder=1)
+    ax.set_xlabel(r'$\log \, N_{\rm HI}$')
+    # End
+    plt.tight_layout(pad=0.2,h_pad=0.,w_pad=0.1)
+    if outfil is not None:
+        print('Writing {:s}'.format(outfil))
+        pp.savefig()
+        pp.close()
+        plt.close()
+    else:
+        plt.show()
 
 
 
