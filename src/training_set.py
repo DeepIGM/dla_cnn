df62168139836a75ae6e964f13f170d8c147fb2e
J. Xavier Prochaska <xavier@ucolick.org>
2016-11-01 09:57:01 -0700

    sightlines

--- src/training_set.py
+++ src/training_set.py
@@ -7,12 +7,91 @@ import numpy as np
 import pdb
 
 
-#from astropy.table import Table
-#from astropy.coordinates import SkyCoord, match_coordinates_sky
-#from astropy import units as u
+from astropy.coordinates import SkyCoord, match_coordinates_sky
+from astropy import units as u
 
 from linetools import utils as ltu
 from pyigm.surveys.dlasurvey import DLASurvey
 
 
-#def grab_clean_sightlines():
+def grab_sightlines(dlas, flg_bal=1, s2n=5., DX=0.):
+    """
+    Parameters
+    ----------
+    dlas : DLASurvey
+      Usually SDSS or BOSS
+    flg_bal : int, optional
+      Maximum BAL flag (0=No signature, 1=Weak BAL, 2=BAL)
+    s2n : float, optional
+      Minimum S/N as defined in some manner
+    DX : float, optional
+      Restrict on DX
+
+    Returns
+    -------
+    final : Table
+      astropy Table of good sightlines
+    sdict : dict
+      dict describing the sightlines
+    """
+    # Init
+    nsight = len(dlas.sightlines)
+    keep = np.array([True]*nsight)
+
+    # Avoid DLAs
+    dla_coord = dlas.coord
+    sl_coord = SkyCoord(ra=dlas.sightlines['RA'], dec=dlas.sightlines['DEC'])
+    idx, d2d, d3d = match_coordinates_sky(sl_coord, dla_coord, nthneighbor=1)
+    clear = d2d > 1*u.arcsec
+    keep = keep & clear
+
+    # BAL
+    if flg_bal > 0:
+        gd_bal = dlas.sightlines['FLG_BAL'] <= flg_bal
+        keep = keep & gd_bal
+
+    # S/N
+    if s2n > 0.:
+        gd_s2n = dlas.sightlines['S2N'] > s2n
+        keep = keep & gd_s2n
+
+    # Cut on DX
+    if DX > 0.:
+        gd_DX = dlas.sightlines['DX'] > DX
+        keep = keep & gd_DX
+
+    # Assess
+    final = dlas.sightlines[keep]
+    sdict = {}
+    sdict['n'] = len(final)
+    print("We have {:d} sightlines for analysis".format(sdict['n']))
+
+    def qck_stats(idict, tbl, istr, key):
+        idict[istr+'min'] = np.min(tbl[key])
+        idict[istr+'max'] = np.max(tbl[key])
+        idict[istr+'median'] = np.median(tbl[key])
+    qck_stats(sdict, final, 'z', 'ZEM')
+    qck_stats(sdict, final, 'i', 'MAG')
+
+    print("Min z = {:g}, Median z = {:g}, Max z = {:g}".format(sdict['zmin'], sdict['zmedian'], sdict['zmax']))
+
+    # Return
+    return final, sdict
+
+
+def main(flg_tst, sdss=None, ml_survey=None):
+
+    # Sightlines
+    if (flg_tst % 2**1) >= 2**0:
+        if sdss is None:
+            sdss = DLASurvey.load_SDSS_DR5(sample='all')
+        slines, sdict = grab_sightlines(sdss)
+
+
+# Test
+if __name__ == '__main__':
+    flg_tst = 0
+    flg_tst += 2**0   # Grab sightlines
+    #flg_tst += 2**1   # Vette
+
+    main(flg_tst)
