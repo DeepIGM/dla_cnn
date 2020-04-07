434a51e7acc6a915c0da1a1ab5ed7b385e418623
Jiaqi Zou <52887789+zoujiaqi99@users.noreply.github.com>
2020-04-07 22:38:51 +0800

    Update training_set.py

--- dla_cnn/training_set.py
+++ dla_cnn/training_set.py
@@ -438,15 +438,16 @@ def save_np_dataset(save_file, data):
 # Receives data in the tuple form returned from split_sightline_into_samples:
 # (fluxes_matrix, classification, offsets_array, column_density)
 # Returns indexes of pos & neg samples that are 50% positive and 50% negative and no boarder
-def select_samples_50p_pos_neg(classification):
+from dla_cnn.spectra_utils import get_lam_data
+def select_samples_50p_pos_neg(sightline,kernel):
     """
     For a given sightline, generate the indices for DLAs and for without
     Split 50/50 to have equal representation
 
     Parameters
     ----------
-    classification: np.ndarray
-        Array of classification values.  1=DLA; 0=Not; -1=not analyzed
+    sightline:dla_cnn.data_model.Sightline.Sightline object
+    kernel:int
 
     Returns
     -------
@@ -454,15 +455,19 @@ def select_samples_50p_pos_neg(classification):
         positive + negative indices
 
     """
-    #classification = data[1]
-    num_pos = np.sum(classification==1, dtype=np.float64)
-    num_neg = np.sum(classification==0, dtype=np.float64)
+  
+    lam, lam_rest, ix_dla_range = get_lam_data(sightline.loglam, sightline.z_qso, REST_RANGE=[900,1346])
+    kernelrangepx = int(kernel/2)
+    cut=((np.nonzero(ix_dla_range)[0])>=kernelrangepx)&((np.nonzero(ix_dla_range)[0])<=(len(lam)-kernelrangepx-1))
+    newclassification=sightline.classification[cut]
+    num_pos = np.sum(newclassification==1, dtype=np.float64)
+    num_neg = np.sum(newclassification==0, dtype=np.float64)
     n_samples = int(min(num_pos, num_neg))
 
-    r = np.random.permutation(len(classification))
+    r = np.random.permutation(len(newclassification))
 
-    pos_ixs = r[classification[r]==1][0:n_samples]
-    neg_ixs = r[classification[r]==0][0:n_samples]
+    pos_ixs = r[newclassification[r]==1][0:n_samples]
+    neg_ixs = r[newclassification[r]==0][0:n_samples]
     # num_total = data[0].shape[0]
     # ratio_neg = num_pos / num_neg
 
