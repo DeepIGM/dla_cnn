37f232a6fc2965d2661aadf465ed8c5a28ad9aee
ZechangSun <60177057+ZechangSun@users.noreply.github.com>
2020-04-17 19:27:15 +0800

    Update preprocess.py

--- dla_cnn/desi/preprocess.py
+++ dla_cnn/desi/preprocess.py
@@ -13,14 +13,16 @@ import numpy as np
 from dla_cnn.spectra_utils import get_lam_data
 from dla_cnn.data_model.DataMarker import Marker
 from scipy.interpolate import interp1d
-
+from os.path import join,exists
+from os import remove
+import csv
 # Set defined items
 #from dla_cnn.desi import defs
 #REST_RANGE = defs.REST_RANGE
 #kernel = defs.kernel
 
 
-def label_sightline(sightline, kernel, REST_RANGE, pos_sample_kernel_percent=0.3):
+def label_sightline(sightline, kernel=400, REST_RANGE=[900,1316], pos_sample_kernel_percent=0.3):
     """
     Add labels to input sightline based on the DLAs along that sightline
 
@@ -43,8 +45,12 @@ def label_sightline(sightline, kernel, REST_RANGE, pos_sample_kernel_percent=0.3
     lam, lam_rest, ix_dla_range = get_lam_data(sightline.loglam, sightline.z_qso, REST_RANGE)
     samplerangepx = int(kernel*pos_sample_kernel_percent/2) #60
     #kernelrangepx = int(kernel/2) #200
-    ix_dlas = [(np.abs(lam[ix_dla_range]-dla.central_wavelength).argmin()) for dla in sightline.dlas]
-    coldensity_dlas = [dla.col_density for dla in sightline.dlas]       # column densities matching ix_dlas
+    ix_dlas=[]
+    coldensity_dlas=[]
+    for dla in sightline.dlas:
+        if 912<(dla.central_wavelength/(1+sightline.z_qso))<1220:
+            ix_dlas.append(np.abs(lam[ix_dla_range]-dla.central_wavelength).argmin()) 
+            coldensity_dlas.append(dla.col_density)    # column densities matching ix_dlas
 
     '''
     # FLUXES - Produce a 1748x400 matrix of flux values
@@ -54,7 +60,7 @@ def label_sightline(sightline, kernel, REST_RANGE, pos_sample_kernel_percent=0.3
 
     # CLASSIFICATION (1 = positive sample, 0 = negative sample, -1 = border sample not used
     # Start with all samples zero
-    classification = np.zeros((REST_RANGE[2]), dtype=np.float32)
+    classification = np.zeros((np.sum(ix_dla_range)), dtype=np.float32)
     # overlay samples that are too close to a known DLA, write these for all DLAs before overlaying positive sample 1's
     for ix_dla in ix_dlas:
         classification[ix_dla-samplerangepx*2:ix_dla+samplerangepx*2+1] = -1
@@ -62,17 +68,17 @@ def label_sightline(sightline, kernel, REST_RANGE, pos_sample_kernel_percent=0.3
         lyb_ix = sightline.get_lyb_index(ix_dla)
         classification[lyb_ix-samplerangepx:lyb_ix+samplerangepx+1] = -1
     # mark out bad samples from custom defined markers
-    for marker in sightline.data_markers:
-        assert marker.marker_type == Marker.IGNORE_FEATURE              # we assume there are no other marker types for now
-        ixloc = np.abs(lam_rest - marker.lam_rest_location).argmin()
-        classification[ixloc-samplerangepx:ixloc+samplerangepx+1] = -1
+    #for marker in sightline.data_markers:
+        #assert marker.marker_type == Marker.IGNORE_FEATURE              # we assume there are no other marker types for now
+        #ixloc = np.abs(lam_rest - marker.lam_rest_location).argmin()
+        #classification[ixloc-samplerangepx:ixloc+samplerangepx+1] = -1
     # overlay samples that are positive
     for ix_dla in ix_dlas:
         classification[ix_dla-samplerangepx:ix_dla+samplerangepx+1] = 1
 
     # OFFSETS & COLUMN DENSITY
-    offsets_array = np.full([REST_RANGE[2]], np.nan, dtype=np.float32)     # Start all NaN markers
-    column_density = np.full([REST_RANGE[2]], np.nan, dtype=np.float32)
+    offsets_array = np.full([np.sum(ix_dla_range)], np.nan, dtype=np.float32)     # Start all NaN markers
+    column_density = np.full([np.sum(ix_dla_range)], np.nan, dtype=np.float32)
     # Add DLAs, this loop will work from the DLA outward updating the offset values and not update it
     # if it would overwrite something set by another nearby DLA
     for i in range(int(samplerangepx+1)):
@@ -96,12 +102,10 @@ def label_sightline(sightline, kernel, REST_RANGE, pos_sample_kernel_percent=0.3
 def rebin(sightline, v):
     """
     Resample and rebin the input Sightline object's data to a constant dlambda/lambda dispersion.
-
     Parameters
     ----------
     sightline: :class:`dla_cnn.data_model.Sightline.Sightline`
     v: float, and np.log(1+v/c) is dlambda/lambda, its unit is m/s, c is the velocity of light
-
     Returns
     -------
     :class:`dla_cnn.data_model.Sightline.Sightline`:
@@ -187,8 +191,7 @@ def normalize(sightline, full_wavelength, full_flux):
     #use the slice we chose above to normalize this spectra, normalize both flux and error array using the same factor to maintain the s/n.
     good_pix = (rest_wavelength>=blue_limit)&(rest_wavelength<=red_limit)
     sightline.flux = sightline.flux/np.median(full_flux[good_pix])
-    sightline.error = sightline.error/np.median(full_flux[good_pix])
-    sightline.normalized = True
+    sightline.error = sightline.error
 
 def estimate_s2n(sightline):
     """
@@ -210,7 +213,7 @@ def estimate_s2n(sightline):
     #lymann forest part of this sightline, contain dlas 
     test = (rest_wavelength>blue_limit)&(rest_wavelength<red_limit)
     #when excluding the part of dla, we remove the part between central_wavelength+-delta
-    dwv = rest_wavelength[1]-rest_wavelength[0]#because we may change the re-sampling of the spectra, this need to be calculated.
+    dwv = rest_wavelength[1]-rest_wavelength[0]
     dv = dwv/rest_wavelength[0] * 3e5  # km/s
     delta = int(np.round(3000./dv))
     for dla in sightline.dlas:
@@ -225,10 +228,11 @@ def generate_summary_table(sightlines, output_dir, mode = "w"):
     Generate a csv file to store some necessary information of the given sightlines. The necessary information means the id, z_qso,
     s/n of thelymann forest part(avoid dlas+- 3000km/s), the wavelength range and corresponding pixel number of each channel. And the csv file's format is like:
     
-    id(int)， z_qso(float), s2n(float), wavelength_start_b(float), wavelength_end_b(float), pixel_start_b(int), pixel_end_b(int), wavelength_start_r(float), wavelength_end_r(float), pixel_start_r(int), pixel_end_r(int), wavelength_start_z(float), wavelength_end_z(float), pixel_start_z(int), pixel_end_z(int)
+    id(int)， z_qso(float), s2n(float), wavelength_start_b(float), wavelength_end_b(float), pixel_start_b(int), pixel_end_b(int), wavelength_start_r(float), wavelength_end_r(float), pixel_start_r(int), pixel_end_r(int), wavelength_start_z(float), wavelength_end_z(float), pixel_start_z(int), pixel_end_z(int),dlas_col_density(str),dlas_central_wavelength(str)
 
     "wavelength_start_b" means the start wavelength value of b channel, "wavelength_end_b" means the end wavelength value of b channel, "pixel_start_b" means the start pixel number of b channel, "pixel_end_b" means the end pixel number of b channel
-    so do the other two channels.
+    so do the other two channels.Besides, "dlas_col_density" means the col_density array of the sightline, and "dlas_central_wavelength" means the central wavelength array means the central wavelength array of the given sightline. Due to the uncertainty of the dlas' number, we chose to use str format to store the two arrays,
+    each array is written in the format like "value1,value2, value3", and one can use `str.split(",")` to get the data, the column density and central wavelength which have the same index in the two arrays corrspond to the same dla.
     -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     parameters:
     sightlines: list of `dla_cnn.data_model.Sightline.Sightline` object, the sightline contained should contain the all data of b,r,z channel, and shouldn't be rebinned,
@@ -239,10 +243,9 @@ def generate_summary_table(sightlines, output_dir, mode = "w"):
     None
     
     """
-    import csv
     #the header of the summary table, each element's meaning can refer to above comment
     headers = ["id","z_qso","s2n","wavelength_start_b","wavelength_end_b","pixel_start_b","pixel_end_b","wavelength_start_r",\
-               "wavelength_end_r","pixel_start_r","pixel_end_r","wavelength_start_z","wavelength_end_z","pixel_start_z","pixel_end_z"]
+               "wavelength_end_r","pixel_start_r","pixel_end_r","wavelength_start_z","wavelength_end_z","pixel_start_z","pixel_end_z","dlas_col_density","dlas_central_wavelength"]
     #open the csv file
     with open(output_dir, mode=mode,newline="") as summary_table:
         summary_table_writer = csv.DictWriter(summary_table,headers)
@@ -255,5 +258,67 @@ def generate_summary_table(sightlines, output_dir, mode = "w"):
                    "wavelength_start_r":10**sightline.loglam[sightline.split_point_br],"wavelength_end_r":10**sightline.loglam[sightline.split_point_rz-1],\
                    "pixel_start_r":sightline.split_point_br,"pixel_end_r":sightline.split_point_rz-1,"wavelength_start_z":10**sightline.loglam[sightline.split_point_rz],\
                    "wavelength_end_z":10**sightline.loglam[-1],"pixel_start_z":sightline.split_point_rz,"pixel_end_z":len(sightline.loglam)-1}
+            
+            dlas_col_density = ""
+            dlas_central_wavelength = ""
+            for dla in sightline.dlas:
+                dlas_col_density += str(dla.col_density)+","
+                dlas_central_wavelength += str(dla.central_wavelength)+","
+            info["dlas_col_density"] = dlas_col_density[:-1]
+            info["dlas_central_wavelength"] = dlas_central_wavelength[:-1]
+            
             #write to the csv file
             summary_table_writer.writerow(info)
+
+def write_summary_table(nums, path, output_path):
+    """
+    Directly read data from fits files and write the summary table, the summary table contains all available sightlines(dlas!=[] and z_qso>2.33) in the given fits files.
+    -----------------------------------------------------------------------------------------------------------------------------------------
+    parameters:
+    nums: list, the given fits files' id, its elements' format is int, and one should make sure all fits files are available before invoking this funciton, otherwise some sightlines can be missed;
+    path: str, the dir of the folder which stores the given fits file, the folder's structure is like folder-fits files' id - fits files , if you are still confused, you can check the below code about read data from the fits file;
+    output_path: str, the dir where the summary table is generated, and if there have been a summary table, then we will remove it and generate a new summary table;
+    ------------------------------------------------------------------------------------------------------------------------------------------
+    retrun:
+    None
+    """
+    #if exists summary table before, remove it
+    if exists(output_path):
+        remove(output_path)
+    def write_as_summary_table(num):
+        """
+        write summary table for a single given fits file, if there have been a summary table then directly write after it, otherwise create a new one
+        ---------------------------------------------------------------------------------------------------------------------------------------------
+        parameter:
+        num: int, the id of the given fits file, e.g. 700
+        ---------------------------------------------------------------------------------------------------------------------------------------------
+        return:
+        None
+        """
+        #read data from fits file
+        file_path = join(path,str(num))
+        spectra = join(file_path,"spectra-16-%s.fits"%num)
+        truth = join(file_path,"truth-16-%s.fits"%num)
+        zbest = join(file_path,"zbest-16-%s.fits"%num)
+        spec = DesiMock()
+        spec.read_fits_file(spectra,truth,zbest)
+        sightlines = []
+        bad_sightlines = []
+        for key in spec.data.keys():
+            if spec.data[key]["z_qso"]>2.33 and spec.data[key]["DLAS"]!=[]:
+                sightlines.append(spec.get_sightline(key))
+        #generate summary table
+        if exists(output_path):
+            generate_summary_table(sightlines,output_path,"a")
+        else:
+            generate_summary_table(sightlines,output_path,"w")
+    bad_files = [] #store the fits files with problems 
+    #for each id in nums, invoking the `write_as_summary_table` funciton
+    for num in nums:
+        try:
+            write_as_summary_table(num)
+        except:
+            #if have problems append to the bad_files
+            bad_files.append(num)
+    assert bad_files==[], "these fits files have some problems, check them please, fits files' id :%s"%str(bad_files)
+
